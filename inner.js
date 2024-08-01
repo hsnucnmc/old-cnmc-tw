702e5bd430fa1cf4480a2bef7c0d9f257c5f5701
wutc <210114@gs.hs.ntnu.edu.tw>
2024-08-01 15:40:42 +0800

    add inner and fix broken link

---
+++ inner.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    anchors: ['title', 'description','class','service'],
+    scrollingSpeed: 500,
+    loopTop:false,
+    loopBottom: true,
+    navigation: true,
+    responsiveWidth: 768
+});
