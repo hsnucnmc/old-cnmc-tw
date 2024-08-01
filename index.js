8646637180c5c94f6e3692c1674ecbb7a56d6933
wutc <210114@gs.hs.ntnu.edu.tw>
2024-08-01 11:27:34 +0800

    upgrade to 4.0.26 and modify some

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 new fullpage('#fullpage', {
+    anchors: ['title', 'description','class','service'],
     scrollingSpeed: 550,
-    loopTop: true,
+    loopTop:false,
     loopBottom: true,
     navigation: true,
     responsiveWidth: 768
